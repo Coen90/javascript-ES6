7a8488fa372ecc62bd126b4ef6108eaf7bd56220
Hyuntae <bht9011@gmail.com>
2022-04-04 07:25:30 +0900

    variables

---
+++ 10.typeof.js
@@ -0,0 +1,16 @@
+// typeof: 데이터 타입을 활인
+// 값을 타입 문자열로 반환
+let variable;
+console.log(typeof variable);
+
+variable = "";
+console.log(typeof variable);
+
+variable = 123; // 할당된 값에 따라 타입이 결정됨!
+console.log(typeof variable);
+
+variable = function () {}; // 할당된 값에 따라 타입이 결정됨!
+console.log(typeof variable);
+
+variable = Symbol(); // 할당된 값에 따라 타입이 결정됨!
+console.log(typeof variable);

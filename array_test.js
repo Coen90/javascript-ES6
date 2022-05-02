b06daf98b25ed78ff196c1a270ec267ae3c07b9f
coen90 <bht9011@gmail.com>
2022-05-02 16:52:53 +0900

    array practice

---
+++ array_test.js
@@ -0,0 +1,18 @@
+let arr = new Array();
+
+arr.push(new Object({ idx: 1, name: "a" }));
+arr.push(new Object({ idx: 2, name: "b" }));
+arr.push(new Object({ idx: 3, name: "c" }));
+
+console.log(arr);
+
+let config = {
+  obj: arr,
+};
+console.log(config);
+
+console.log(0 % 2);
+const zero = parseInt("0");
+
+console.log("0" % 2);
+console.log(zero % 2);
